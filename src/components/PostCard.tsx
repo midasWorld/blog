07099c2f0a722f65f27e807d6c8a9bb076a96acf
midasworld <midas117@knou.ac.kr>
2023-04-13 02:54:24 +0900

    refactor: PostCard 컴포넌트 수정(`li` 태그 제거)

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -8,24 +8,22 @@ export default function PostCard({
   post: Post;
 }) {
   return (
-    <li>
-      <Link href={`/posts/${id}`}>
-        <Image
-          className="w-full rounded-t-md"
-          src={imageUrl}
-          alt={title}
-          width={300}
-          height={200}
-        />
-        <div className="flex flex-col items-center p-2 gap-0.5">
-          <time className="self-end text-sm text-gray-600">
-            {date.toString()}
-          </time>
-          <h3 className="font-semibold text-lg">{title}</h3>
-          <p className="w-full text-center truncate">{description}</p>
-          <span className="bg-lime-100 px-2 rounded-lg mt-1">{category}</span>
-        </div>
-      </Link>
-    </li>
+    <Link href={`/posts/${id}`}>
+      <Image
+        className="w-full rounded-t-md"
+        src={imageUrl}
+        alt={title}
+        width={300}
+        height={200}
+      />
+      <div className="flex flex-col items-center p-2 gap-0.5">
+        <time className="self-end text-sm text-gray-600">
+          {date.toString()}
+        </time>
+        <h3 className="font-semibold text-lg">{title}</h3>
+        <p className="w-full text-center truncate">{description}</p>
+        <span className="bg-lime-100 px-2 rounded-lg mt-1">{category}</span>
+      </div>
+    </Link>
   );
 }
