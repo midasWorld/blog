cf09172fd9b217ad7c47e9097783fecbec8ea3a1
midasworld <midas117@knou.ac.kr>
2023-04-13 00:54:42 +0900

    feat: 게시글 내용(Markdown File) 보기 기능 구현 - Markdown 관련 의존성 추가 - react-markdown, react-syntax-highlighter, remark-gfm - `tailwindcss/typography` 의존성 추가 및 설정 - MarkdownViewer 컴포넌트 생성 - `/service/posts`: 게시글 내용 파일(`.md`) 조회 기능 구현

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,9 +1,19 @@
+import MarkdownViewer from "@/components/MarkdownViewer";
+import { getPost } from "@/service/posts";
+
 type Props = {
   params: {
     slug: string;
   };
 };
 
-export default function PostPage({ params: { slug } }: Props) {
-  return <h1>{`${slug}번 글!`}</h1>;
+export default async function PostPage({ params: { slug } }: Props) {
+  const post = await getPost(slug);
+  const { content } = post;
+
+  return (
+    <>
+      <MarkdownViewer content={content} />
+    </>
+  );
 }
---
+++ src/components/MarkdownViewer.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialOceanic } from "react-syntax-highlighter/dist/esm/styles/prism";
+import remarkGfm from "remark-gfm";
+
+export default function MarkdownViewer({ content }: { content: string }) {
+  return (
+    <ReactMarkdown
+      className="prose lg:prose-xl"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              style={materialOceanic}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
+      {content}
+    </ReactMarkdown>
+  );
+}
