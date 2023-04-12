42a95d321a876b01090d6b66eef50a53df490cc1
midasworld <midas117@knou.ac.kr>
2023-04-13 00:54:42 +0900

    feat: 게시글 내용(Markdown File) 보기 기능 구현 - Markdown 관련 의존성 추가 - react-markdown, react-syntax-highlighter, remark-gfm - `tailwindcss/typography` 의존성 추가 및 설정 - MarkdownViewer 컴포넌트 생성 - `/service/posts`: 게시글 내용 파일(`.md`) 조회 기능 구현

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -9,6 +9,11 @@ export type Post = {
   category: string;
   imageUrl: string;
   featured: boolean;
+  filePath: string;
+};
+
+export type PostData = Post & {
+  content: string;
 };
 
 export async function getFeaturedPosts(): Promise<Post[]> {
@@ -20,4 +25,18 @@ export async function getPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), "data", "posts.json");
   return readFile(filePath, "utf-8") //
     .then<Post[]>(JSON.parse);
+}
+
+export async function getPost(id: string): Promise<PostData> {
+  const posts = await getPosts();
+  const post = posts.filter((post) => post.id === id)[0];
+
+  if (!post) {
+    throw new Error(`Post is not exists. id: ${id}`);
+  }
+
+  const filePath = path.join(process.cwd(), "data/posts", post.filePath);
+  const content = await readFile(filePath, "utf-8");
+
+  return { ...post, content };
 }
