c358fb6c77a9b186c419ed13c34a16d05d3687b9
midasworld <midas117@knou.ac.kr>
2023-04-14 01:35:18 +0900

    feat: Footer 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import { Open_Sans } from "next/font/google";
+import Footer from "@/components/Footer";
 
 const sans = Open_Sans({ subsets: ["latin"] });
 
@@ -22,7 +23,8 @@ export default function RootLayout({
     <html lang="en" className={sans.className}>
       <body className="flex flex-col w-full max-w-screen-2xl mx-auto">
         <Header />
-        {children}
+        <main className="grow">{children}</main>
+        <Footer />
       </body>
     </html>
   );
