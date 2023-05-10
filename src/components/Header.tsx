1be520eae6eef77c17ae8d55e22e2a014ba3bb3e
midasworld <midas117@knou.ac.kr>
2023-05-11 07:11:36 +0900

    feat: 구글 로그인/로그아웃 구현 - next-auth 의존성 추가 - AuthContext 생성 (session 사용을 위해) - 로그인, 로그아웃 버튼 컴포넌트 생성 - Header 에 추가(session 따라 로그인/로그아웃 버튼 활성화)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import SignInButton from "./ui/SignInButton";
+import { useSession } from "next-auth/react";
+import SignOutButton from "./ui/SignOutButton";
 
 export default function Header() {
+  const { data: session } = useSession();
   return (
     <header className="flex justify-between items-center w-full p-4">
       <h1 className="text-3xl font-semibold">
@@ -11,6 +17,7 @@ export default function Header() {
         <Link href="/about">about</Link>
         <Link href="/posts">posts</Link>
         <Link href="/contact">contact</Link>
+        {session ? <SignOutButton /> : <SignInButton />}
       </nav>
     </header>
   );
