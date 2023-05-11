f4e75d830f7dd12997e6dc30fb552c8fb67fdaf4
midasworld <midas117@knou.ac.kr>
2023-05-11 15:57:44 +0900

    feat: next-auth adapter - prisma 연동 - prisma 의존성 추가 및 스키마 설정

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,14 @@
+import { prisma } from "@/db/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID || "",
